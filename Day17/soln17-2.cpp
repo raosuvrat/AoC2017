f3a51b66fc8891e54d851a2e1c1c247e921715d1
raosuvrat <raosuvrat@users.noreply.github.com>
2017-12-17 17:44:43 -0600

    Day 17 changes

--- Day17/soln17-2.cpp
+++ Day17/soln17-2.cpp
@@ -5,11 +5,11 @@ using namespace std;
 int main() {
     vector<int> vec;
     vec.push_back(0);
-    vec.reserve(2020);
-    for (int i = 1; i <= 2017; ++i) {
+    vec.reserve(50000000);
+    for (int i = 1; i <= 50000000; ++i) {
         rotate(vec.begin(), vec.begin() + 344%vec.size(), vec.end());
         vec.push_back(i);
     }
-    auto it = find(vec.begin(), vec.end(), 2017);
+    auto it = find(vec.begin(), vec.end(), 0;
     cout << vec[(it - vec.begin() + 1)%vec.size()] << endl;
 }
