66ec94d3c62d1b970ce0eefa68b229e0035c7696
raosuvrat <raosuvrat@users.noreply.github.com>
2017-12-17 16:27:09 -0600

    Day 17

---
+++ Day17/soln17-1.cpp
@@ -0,0 +1,15 @@
+#include<vector>
+#include<iostream>
+#include<algorithm>
+using namespace std;
+int main() {
+    vector<int> vec;
+    vec.push_back(0);
+    vec.reserve(50000000);
+    for (int i = 1; i <= 50000000; ++i) {
+        rotate(vec.begin(), vec.begin() + 344%vec.size(), vec.end());
+        vec.push_back(i);
+    }
+    auto it = find(vec.begin(), vec.end(), 0);
+    cout << vec[it - vec.begin() + 1] << endl;
+}
