90f658fc63234c5828884111a0489fc7c255b509
raosuvrat <raosuvrat@users.noreply.github.com>
2017-12-17 18:46:33 -0600

    Day 17 Part 2 Fast

--- Day17/soln17-2.cpp
+++ Day17/soln17-2.cpp
@@ -1,15 +1,12 @@
 #include<vector>
 #include<iostream>
 #include<algorithm>
+#define STEPS 344
 using namespace std;
 int main() {
-    vector<int> vec;
-    vec.push_back(0);
-    vec.reserve(50000000);
-    for (int i = 1; i <= 50000000; ++i) {
-        rotate(vec.begin(), vec.begin() + 344%vec.size(), vec.end());
-        vec.push_back(i);
-    }
-    auto it = find(vec.begin(), vec.end(), 0;
-    cout << vec[(it - vec.begin() + 1)%vec.size()] << endl;
+    int result;
+    for (int pos = 0, i = 1; i <= 50000000; ++i, pos = (pos+STEPS)%i)
+        if (!(pos++)) result = i;
+    cout << result << endl;
+    return 0;
 }
