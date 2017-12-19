89c79f62c7dc46a8884f95cc3f1a7a16dd4e9f55
raosuvrat <raosuvrat@users.noreply.github.com>
2017-12-18 20:03:53 -0600

    retab

--- Day18/soln18-2.cpp
+++ Day18/soln18-2.cpp
@@ -7,17 +7,17 @@
 using namespace std;
 
 struct Machine {
-	Machine *partner;
-	vector<string> rom;
-	queue<long long> out; int sent = 0;
-	int pc = 0, id;
-	map<string, long long> regfile;
-	Machine(vector<string> input, int id) : rom(input), id(id) { regfile["p"] = id; }
-	bool step() {
-		string op, arg1, arg2;
-		stringstream ss(rom[pc]);
-		ss >> op >> arg1 >> arg2;
-		if (!regfile.count(arg1)) regfile[arg1] = 0;
+    Machine *partner;
+    vector<string> rom;
+    queue<long long> out; int sent = 0;
+    int pc = 0, id;
+    map<string, long long> regfile;
+    Machine(vector<string> input, int id) : rom(input), id(id) { regfile["p"] = id; }
+    bool step() {
+        string op, arg1, arg2;
+        stringstream ss(rom[pc]);
+        ss >> op >> arg1 >> arg2;
+        if (!regfile.count(arg1)) regfile[arg1] = 0;
         if (op == "snd") {
             out.push(regfile[arg1]); ++sent;
         } if (op == "set") {
@@ -29,15 +29,15 @@ struct Machine {
         } if (op == "mod") {
             regfile[arg1] %= isalpha(arg2[0]) ? regfile[arg2] : atoi(arg2.c_str());
         } if (op == "rcv") {
-			if (partner->out.empty()) return false;
-			regfile[arg1] = partner->out.front(); partner->out.pop();
+            if (partner->out.empty()) return false;
+            regfile[arg1] = partner->out.front(); partner->out.pop();
         } if (op == "jgz") {
             if ((isalpha(arg1[0]) ? regfile[arg1] : atoi(arg1.c_str())) > 0)
                 pc += (isalpha(arg2[0]) ? regfile[arg2] : atoi(arg2.c_str()))-1;
         }
-		++pc;
-		return (pc >= 0 && pc < rom.size());
-	}
+        ++pc;
+        return (pc >= 0 && pc < rom.size());
+    }
 };
 
 int main() {
@@ -47,9 +47,9 @@ int main() {
         string s("");
         if(getline(infile, s)) input.push_back(s);
     }
-	Machine a(input, 0), b(input, 1);
-	a.partner = &b; b.partner = &a;
-	while (a.step() || b.step());
-	cout << "a sent: " << a.sent << " b sent: " << b.sent << endl;
+    Machine a(input, 0), b(input, 1);
+    a.partner = &b; b.partner = &a;
+    while (a.step() || b.step());
+    cout << "a sent: " << a.sent << " b sent: " << b.sent << endl;
     return 0;
 }
