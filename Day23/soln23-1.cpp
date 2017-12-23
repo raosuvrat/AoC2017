e2e03c7f4a679b905135b6703e7aecc17bfe0f82
raosuvrat <raosuvrat@users.noreply.github.com>
2017-12-23 00:44:22 -0600

    Day 23 Solutions

---
+++ Day23/soln23-1.cpp
@@ -0,0 +1,35 @@
+#include<map>
+#include<fstream>
+#include<vector>
+#include<sstream>
+#include<iostream>
+
+using namespace std;
+int main() {
+    fstream infile("input");
+    vector<string> input;
+    while (!infile.eof()){
+        string s("");
+        if(getline(infile, s)) input.push_back(s);
+    }
+    int mul =0;
+    string op, arg1, arg2;
+    map<string, long long> regfile;
+    for (long long i = 0; i >= 0 && i < input.size(); ++i){
+        stringstream ss(input[i]);
+        ss >> op >> arg1 >> arg2;
+        if (!regfile.count(arg1)) regfile[arg1] = 0;
+        if (op == "set") {
+            regfile[arg1] = isalpha(arg2[0]) ? regfile[arg2] : atoi(arg2.c_str());
+        } if (op == "sub") {
+            regfile[arg1] -= isalpha(arg2[0]) ? regfile[arg2] : atoi(arg2.c_str());
+        } if (op == "mul") { mul++;
+            regfile[arg1] *= isalpha(arg2[0]) ? regfile[arg2] : atoi(arg2.c_str());
+        } if (op == "jnz") {
+            if ((isalpha(arg1[0]) ? regfile[arg1] : atoi(arg1.c_str())) != 0)
+            i += (isalpha(arg2[0]) ? regfile[arg2] : atoi(arg2.c_str()))-1;
+        }
+    }
+    cout << mul << endl;
+    return 0;
+}
---
+++ Day23/soln23-2.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include<math.h>
+using namespace std;
+int main() {
+    int h = 0;
+    for (int i = 107900; i <= (107900+17000); i += 17)
+        for (int j = 2; j < i; ++j)
+            if (!(i % j)) { h++; break; }
+    cout << h << endl;
+}
+
